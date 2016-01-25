570f283986bfd458b8fce7d6ca8d093176a5e072
timglabisch <tim@ainfach.de>
2016-01-25 23:33:50 +0100

    fix noassoc

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,13 @@ fn parse_expr(expr : &str) -> String {
 
 fn main() {
 
+    assert_eq!("1", parse_expr("1"));
+    assert_eq!("(++1)", parse_expr("++1"));
+    assert_eq!("(++1)", parse_expr("++(1)"));
+    assert_eq!("(++1)", parse_expr("++ 1"));
+    assert_eq!("(++1)", parse_expr("++ (1)"));
+
+
     assert_eq!("((1 + 2) + 3)", parse_expr("1 + 2 + 3"));
     assert_eq!("((1 - 2) - 3)", parse_expr("1 - 2 - 3"));
     assert_eq!("((1 * 2) * 3)", parse_expr("1 * 2 * 3"));
@@ -24,9 +31,14 @@ fn main() {
     assert_eq!("((1 * 2) - 3)", parse_expr("1 * 2 - 3"));
 
     assert_eq!("(((1 / 2) + 3) == (1 + (2 * 3)))", parse_expr("1 / 2 + 3 == 1 + 2 * 3"));
-    assert_eq!("((((1 / 2) + 3) == (1 + (2 * 3))) == (1 + (2 * 3)))", parse_expr("1 / 2 + 3 == 1 + 2 * 3 == 1 + 2 * 3"));
+
+    assert!(calculator1::parse_expr("1 == 1").is_ok());
+    assert!(calculator1::parse_expr("((1 / 2) + 3) == ((1 * 2) + 3)").is_ok());
+    assert!(calculator1::parse_expr("((1 + 2) + 3) == ((1 - 2) + 3)").is_ok());
+    assert!(calculator1::parse_expr("1 == 1 == 1").is_err());
 
     // testing ++
-    assert_eq!("(((((1 + (++2)) + ((3 * 4) * (++5))) + 6) == ((23 * 2) + 1)) == 45)", parse_expr("1 + ++2 + 3 * 4 * ++5 + 6 == 23 * 2 + 1 == 45"));
+    assert_eq!("(((1 + (++2)) + ((3 * 4) * (++5))) + 6)", parse_expr("1 + ++2 + 3 * 4 * ++5 + 6"));
     assert_eq!("((++(1 + (++2))) * 3)", parse_expr("++(1 + ++2) * 3"));
+    assert_eq!("((++(1 + (++2))) * 3)", parse_expr("++ (1 + ++2) * 3"));
 }
